28bd6009388cf2e4dbc51ccdbc16afd04093dd43
Wolfgang Meier <wolfgang@existsolutions.com>
2022-05-08 10:22:26 +0200

    demo: update generated files

--- dist/index-7bab7f25.d.ts
+++
@@ -1,4 +0,0 @@
-import { Tree } from "@lezer/common";
-declare function leiden2epiDoc(input: string): string;
-declare function syntax2epiDoc(root: Tree, input: string): string;
-export { leiden2epiDoc, syntax2epiDoc };
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-export{l as leiden2epiDoc,s as syntax2epiDoc}from"./index-7bab7f25.js";
+export{l as leiden2epiDoc,s as syntax2epiDoc}from"./index-92306718.js";
