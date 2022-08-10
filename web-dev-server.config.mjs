7def3b98fbfac27df4381f56e3b258cb49707d9b
Wolfgang Meier <wolfgang@exist-db.org>
2022-08-10 10:55:26 +0200

    Add access control header to simplify development

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,16 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 export default {
-  plugins: [esbuildPlugin({ ts: true })],
+  plugins: [
+    {
+      name: 'xhr',
+      transform(context) {
+        context.response.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+        return {
+            body: context.body.replace(/process.env.NODE_ENV/g, JSON.stringify('production'))
+        };
+      }
+    },
+    esbuildPlugin({ ts: true })
+  ],
 };
